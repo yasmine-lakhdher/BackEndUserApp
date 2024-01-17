e22ddd28bb37ddad0a61c4be48f5f5aea936d67c
yasmine-lakhdher <131556886+yasmine-lakhdher@users.noreply.github.com>
2024-01-17 21:36:33 +0100

    Update admin.java

--- lms-v2/src/main/java/com/example/demo/model/admin.java
+++ lms-v2/src/main/java/com/example/demo/model/admin.java
@@ -3,7 +3,7 @@ package com.example.demo.model;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "Users")
+@Table(name = "userstable")
 public class admin {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
