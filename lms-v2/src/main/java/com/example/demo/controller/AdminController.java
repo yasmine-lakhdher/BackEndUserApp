9a56320608ce6aac9ecd4ae4afb8b5c79d028402
yasmine-lakhdher <131556886+yasmine-lakhdher@users.noreply.github.com>
2024-01-17 22:08:31 +0100

    Update AdminController.java

--- lms-v2/src/main/java/com/example/demo/controller/AdminController.java
+++ lms-v2/src/main/java/com/example/demo/controller/AdminController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://172.17.0.2:4200")
 @RestController
 
 public class AdminController {
